c6b254c55b39a914edc7fced8ad232f52cd4aacf
sangeeth-606 <sangeeth999123@gmail.com>
2025-01-18 12:59:31 +0000

    add validation for todo creation and update using Zod

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
-const express= require("express")
+const express= require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 
 app.use(express.json());
 
 app.post("/todo",function (req,res){
+
+    const createPayload=req.body;
+    const parsedPayload=createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"you sent the wrong inputs"
+        })
+        return;
+    }
 
 } )
 
@@ -12,5 +22,13 @@ app.get("/todos" ,function(req,res){
 } )
 
 app.put("/completed", function(req,res){
+    const updatePayload=req.body;
+    const parsedPayload=updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"you sent the wrong inputs"
+        })
+        return;
+    }
 
 } )
